ed53e3cbcacb962aa95332b2e9ee035767f5aed5
sitimarfungahdwil <sitimarfungahdl@gmail.com>
2025-09-20 11:32:27 +0700

    menambahkan fitur authentikasi dengan jetstream

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Admin Dashboard</title>
+    </head>
+    <body>
+        <x-app-layout>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Admin Dashboard') }}
+                </h2>
+            </x-slot>
+
+
+        </x-app-layout>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/home', [AdminController::class, 'index'])->name('home');
