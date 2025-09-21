b66816abf3dfc81593734536fd12f64892e87874
sitimarfungahdwil <sitimarfungahdl@gmail.com>
2025-09-21 08:53:26 +0700

    Adding admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Admin Dashboard</title>
-    </head>
-    <body>
-        <x-app-layout>
-            <x-slot name="header">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ __('Admin Dashboard') }}
-                </h2>
-            </x-slot>
-
-
-        </x-app-layout>
-    </body>
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <header class="header">
+      @include('admin.header')
+    </header>
+    @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      @include('admin.body')
+        @include('admin.footer')
+  </body>
 </html>
