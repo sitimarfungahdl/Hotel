36b88491b7773d1a3f8f5315f8403e2e1589f688
sitimarfungahdwil <sitimarfungahdl@gmail.com>
2025-09-25 00:41:03 +0700

    add approve and rejected booking

--- resources/views/admin/booking.blade.php
+++ resources/views/admin/booking.blade.php
@@ -6,13 +6,13 @@
         .table_deg{
             border: 2px solid white;
             margin: auto;
-            width: 100%;
+            width: 80%;
             text-align: center;
             margin-top: 40px;
         }
         .th_deg{
             background-color: skyblue;
-            padding: 15px;
+            padding: 8px;
         }
         tr
         {
@@ -46,6 +46,7 @@
                     <th class="th_deg">Price</th>
                     <th class="th_deg">Image</th>
                     <th class="th_deg">Delete</th>
+                    <th class="th_deg">Status Update</th>
                 </tr>
                 @foreach ($data as $data)
                 <tr>
@@ -55,7 +56,17 @@
                     <td>{{ $data->phone }}</td>
                     <td>{{ $data->start_date }}</td>
                     <td>{{ $data->end_date }}</td>
-                    <td>{{ $data->status }}</td>
+                    <td>
+                        @if($data->status == 'approve')
+                        <span style="color: green">Approved</span>
+                        @endif
+                        @if($data->status == 'rejected')
+                        <span style="color: red">Rejected</span>
+                        @endif
+                        @if($data->status == 'waiting')
+                        <span style="color: orange">Waiting</span>
+                        @endif
+                    </td>
                     <td>{{ $data->room->room_title }}</td>
                     <td>{{ $data->room->price }}</td>
                     <td>
@@ -63,6 +74,10 @@
                     </td>
                     <td>
                         <a onclick="return confirm('Are you sure to delete this?');" class="btn btn-danger" href="{{ url('delete_booking', $data->id) }}">Delete</a>
+                    </td>
+                    <td>
+                       <span style="padding-bottom: 10px"><a class="btn btn-success" href="{{ url('approve_book', $data->id) }}">Approve</a></span>
+                        <a class="btn btn-warning" href="{{ url('reject_book', $data->id) }}">Rejected</a>
                     </td>
                 </tr>
                 @endforeach
